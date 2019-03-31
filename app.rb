ab1a5416f4266c612abc82cb8b08e62cd206500d
Artem Bondarets <tyom.bond@gmail.com>
2019-03-31 17:45:56 +0300

    Init database, make @db, add .gitignore

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'firstblog.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+
+end
 
 get '/' do
 	erb 'Hello!'
